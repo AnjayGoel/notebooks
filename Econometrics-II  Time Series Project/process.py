b7f863137b62dca27d17176700fdbbe3454e9825
AnjayGoel <angoel23@gmail.com>
2021-11-07 15:03:00 +0530

    Add Risk Free Rate Scraper

--- Econometrics-II  Time Series Project/process.py
+++ Econometrics-II  Time Series Project/process.py
@@ -1,9 +1,14 @@
 # %%
+import datetime
+import math
 import os
 from datetime import datetime, date, timedelta
 import calendar
+
+import numpy as np
 import pandas as pd
 from tabulate import tabulate
+from py_vollib.black_scholes.implied_volatility import *
 
 
 # %%
@@ -12,8 +17,19 @@ def str_to_date(date_string):
     return datetime.strptime(date_string, "%Y-%m-%d").date()
 
 
+def str_to_date_2(date_string):
+    return datetime.strptime(date_string, "%d-%b-%Y").date()
+
+
 def get_last_expiry_date(df: pd.DataFrame):
-    return sorted(df["Expiry"].unique())[-1]
+    sorted_dates = sorted(df["Expiry"].unique())
+    print(sorted_dates)
+    last_date = sorted_dates[-1]
+    if (last_date.year == 2014 and last_date.month == 2) or (
+            last_date.year == 2018 and last_date.month == 3):  # Holidays
+        return sorted_dates[-2]
+    else:
+        return sorted(df["Expiry"].unique())[-1]
 
 
 def get_days_in_month(dt: date):
@@ -35,40 +51,85 @@ def get_prev_month_monday(dt: date):
 
 df_nifty = pd.read_csv("csv/nifty.csv")
 df_nifty["Date"] = df_nifty["Date"].apply(str_to_date)
+df_nifty["Close_Shift"] = df_nifty["Close"].shift(1)
+df_nifty["Close_Shift"] = df_nifty["Close"].shift(1)
+df_nifty["return"] = np.log(df_nifty["Close"] / df_nifty["Close_Shift"])
+df_mibor = pd.read_csv("csv/mibor.csv")
+df_mibor["Date"] = df_mibor["Date"].apply(str_to_date_2)
+
+
+# print(tabulate(df_nifty.tail(50), headers=df_nifty.columns))
+
+
+def get_mibor_mean(dt: date):
+    df_t = df_mibor[df_mibor["Date"].map(lambda dt_curr: dt_curr.year == dt.year and dt_curr.month == dt.month)]
+    return round(df_t["3_Month"].mean(), 2)
+
+
+def get_nifty_std(dt_start: date, dt_end: date):  # Annualized
+    df_t = df_nifty[(df_nifty["Date"] >= dt_start) & (df_nifty["Date"] <= dt_end)]
+    return df_t["return"].std() * math.sqrt(252)
 
 
 def get_nifty(dt: date):
     return df_nifty[df_nifty["Date"] == dt].iloc[0]["Close"]
 
 
+def get_implied_volatility(row):
+    V = row["Close"]
+    F = row["Underlying"]
+    K = row["Strike Price"]
+    r = row["Interest_Rate_(Mibor)"] / 100
+    t = np.busday_count(row["Date"], row["Expiry"]) / 252
+    flag = "c"
+    print((V, F, K, t, r, flag))
+    return implied_volatility(V, F, K, t, r, flag)
+
+
 # %%
-def process(year, month):
+def process_row(year, month):
     df = pd.read_csv(f"csv/nifty/{year}-{month}.csv")
     df["Expiry"] = df["Expiry"].apply(str_to_date)
     df["Date"] = df["Date"].apply(str_to_date)
     # print(tabulate(df.sample(10), headers=df.columns))
-    prev_month_date = get_prev_month_monday(get_last_expiry_date(df))
-    # print(prev_month_date)
-    final_date = sorted(df[(df["Date"] >= prev_month_date)]["Date"].unique())[0]
-    # print(final_date)
-    underlying_price = get_nifty(final_date)
-    df = df[(df["Date"] == final_date) & (underlying_price >= df["Strike Price"])]
+    latest_expiry = get_last_expiry_date(df)
+    df = df[df["Expiry"] == latest_expiry]
+    prev_month_date = get_prev_month_monday(latest_expiry)
+    print(prev_month_date)
+    last_month_date = sorted(df[(df["Date"] >= prev_month_date)]["Date"].unique())[0]
+    print(last_month_date)
+    underlying_price = get_nifty(last_month_date)
+    df = df[(df["Date"] == last_month_date) & (underlying_price >= df["Strike Price"])]
     df = df.sort_values(by=['Turnover'])
-    df["Underlying"] = underlying_price
-    return df.iloc[-1]
+    ret_val = df.iloc[-1]
+    ret_val["Underlying"] = underlying_price
+    ret_val["Interest_Rate_(Mibor)"] = get_mibor_mean(ret_val["Date"])
+    ret_val["Realized_Volatility"] = get_nifty_std(last_month_date, latest_expiry)
+    ret_val["Implied_Volatility"] = get_implied_volatility(ret_val)
+
+    return ret_val
     # print(tabulate(df, headers=df.columns))
     # print(underlying_price)
     # print(final_date)
 
 
 # %%
-print(process(2015, 9))
+df = pd.read_csv("csv/nifty/2010-1.csv")
+df = df[df["Underlying"] == -1]
+for year in range(2010, 2020):
+    for month in range(1, 13):
+        # print(process(year, month))
+        df = df.append(process_row(year, month))
+print(tabulate(df, headers=df.columns))
+df.to_csv("csv/final_dataset.csv", index=False)
 
 # %%
+# -----------------------------------------------------
 # Scratch
-
 df = pd.read_csv("csv/2018-09.csv")
 last_date = get_last_expiry_date(df)
 print(last_date)
 print(get_prev_month_monday(last_date))
 print(get_prev_month_monday(last_date).weekday())
+# %%
+print(get_mibor_mean(date(year=2020, month=11, day=20)))
--- Econometrics-II  Time Series Project/scrape.py
+++ Econometrics-II  Time Series Project/scrape.py
@@ -3,10 +3,13 @@ import time
 from datetime import timedelta
 from itertools import product
 from multiprocessing import *
-
+from lxml import html
 import pandas as pd
+import requests
 from nsepy import *
 from nsepy.derivatives import get_expiry_date
+from requests.structures import CaseInsensitiveDict
+from tabulate import tabulate
 
 logging.basicConfig(
     filename="logs.log",
@@ -16,8 +19,51 @@ logging.basicConfig(
 )
 logging.getLogger().addHandler(logging.StreamHandler())
 
+request_cookies = {
+    "cookieToken": "\"DWPRODAPP2:6400@78488016JhYoC5ZkKdOVn3op6vwgbL378488015Jeu0NNTziNe3n6GM5oYG5LN\"",
+    "InfoViewPLATFORMSVC_COOKIE_TOKEN": "\"DWPRODAPP2:6400@78488016JhYoC5ZkKdOVn3op6vwgbL378488015Jeu0NNTziNe3n6GM5oYG5LN\"",
+    "JSESSIONID": "Urz44QVTClZz5kEqZqwuPHx_LA2OXBg_p0jKT-JUadYWVafrFQ6f!-838047046",
+    "OpenDocumentPLATFORMSVC_COOKIE_TOKEN": "",
+    "TS01f9b35c": "01d966ad3eb132758c46c5ff246595a61cebf666aef91de58f761a7cef8a1d5458124549056b6996c2362ca7e0b457cd65aeb89cd0fbf5ed75f46772282df867b59d177d94c4bc9100b3df56f1a68d7f8abcd8d353acdefa9d3475664742af21c26f652fb375bccb16544e8292ef6547746a4988dd28c4672a7b63f4202e6f44df14dccf6dd9cf601a71de8e17bc3cb5a1e1cd5500",
+    "TS1776a086027": "082160391eab2000193d3663b08c68147ac7d1c459fd70c5908c466995949a44ec01e427ddf4178708e73f382211300011f7db824e212d2c4bf711a4d774ebdba30e6933d0698fc10f344979572d1ded819dc61f62380e12d649f1b15b5ce05f",
+    "VINTELASSO": "\"true\""
+}
 
-def fetch(args):
+
+def save_resp(resp, fn):
+    f = open(f"{fn}.html", "w+")
+    f.write(resp.text)
+    f.close()
+
+
+def fetch_mibor(page_no: int):
+    url = f"https://dbie.rbi.org.in/BOE/OpenDocument/1608101729/AnalyticalReporting/webiDHTML/viewer/report.jsp?iViewerID=3&sEntry=we000500009e9df6a65fe8&iReport=0&iReportID=1&sPageMode=QuickDisplay&sReportMode=Viewing&iPage={page_no}&zoom=100&isInteractive=false&isStructure=false&sBid=&iVSlot={page_no}&iHSlot=0&sUndoEnabled=false&nbPage=NaN"
+
+    headers = CaseInsensitiveDict()
+    headers["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0"
+    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+    headers["Accept-Language"] = "en-US,en;q=0.5"
+    headers["DNT"] = "1"
+    headers["Connection"] = "keep-alive"
+    headers[
+        "Referer"] = "https://dbie.rbi.org.in/BOE/OpenDocument/1608101729/AnalyticalReporting/webiDHTML/viewer/viewDocument.jsp"
+    headers[
+        "Cookie"] = """VINTELASSO="true";cookieToken="DWPRODAPP2:6400@78488016JhYoC5ZkKdOVn3op6vwgbL378488015Jeu0NNTziNe3n6GM5oYG5LN";InfoViewPLATFORMSVC_COOKIE_TOKEN="DWPRODAPP2:6400@78488016JhYoC5ZkKdOVn3op6vwgbL378488015Jeu0NNTziNe3n6GM5oYG5LN"; JSESSIONID=Urz44QVTClZz5kEqZqwuPHx_LA2OXBg_p0jKT-JUadYWVafrFQ6f!-838047046; TS01f9b35c=01d966ad3e84209f048b8657e5230ae8b0fac6afd23c1e1891b743f3628460b2240db7eaed44b6c1bd8586b8f2f28e178aee761259e5662ce981caa91344d6f9c650636d681d8594e28015dc74f3bf4d7f201e8e980b43cdf60b3b52ef789c0ef83dbba90ae9e8343c097c5ea64d0815c4712359ceec8062038f272d6af337b8e35e4ab4045dd3ede82c1e44b668be9c24d6862405; TS1776a086027=082160391eab2000f00ae558c923bb141853a360034ffb496650a9fcc4ba397b2628a3d79b5d0cca085c4d8635113000baf1f7a8471f070683ec70c2abbd44358493b05a2b9e1aa6efd5dace8aee1cff7944fcd0a09e7047979f93628ea130ca; OpenDocumentPLATFORMSVC_COOKIE_TOKEN="""
+    headers["Upgrade-Insecure-Requests"] = "1"
+    headers["Sec-Fetch-Dest"] = "iframe"
+    headers["Sec-Fetch-Mode"] = "navigate"
+    headers["Sec-Fetch-Site"] = "same-origin"
+    headers["Sec-Fetch-User"] = "?1"
+    headers["Sec-GPC"] = "1"
+
+    resp = requests.get(url, headers=headers)
+    df = pd.read_html(resp.text)[0]
+    df = df[[0, 3, 7, 11, 15]]
+    df.columns = ["Date", "Overnight", "14_Days", "1_Month", "3_Month"]
+    return df
+
+
+def fetch_nifty_options_data(args):
     year = args[0]
     month = args[1]
     logging.info(f"Start {year}-{month}")
@@ -50,9 +96,24 @@ def fetch(args):
         logging.error(f"Error: {year}-{month}")
         logging.error(e, exc_info=True)
         time.sleep(1)
-        fetch(args)
+        fetch_nifty_options_data(args)
+
+
+def process_nifty_options():
+    pool = Pool(processes=8)
+    pool.map(fetch_nifty_options_data, product([i for i in range(2010, 2020)], [i for i in range(1, 13)]))
+
+
+def process_mibor():
+    pool = Pool(processes=8)
+    results = pool.map(fetch_mibor, range(1, 100))
+    df_mibor = pd.concat(results).drop_duplicates()
+    pool.close()
+    pool.join()
+    df_mibor.to_csv("csv/mibor.csv", index=False)
 
 
 if __name__ == "__main__":
-    pool = Pool(processes=8)
-    pool.map(fetch, product([i for i in range(2010, 2020)], [i for i in range(1, 13)]))
+    # df = fetch_mibor(1)
+    # print(df)
+    process_mibor()
